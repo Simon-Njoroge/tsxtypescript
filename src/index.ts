0ad69ba38f660b2129b32e5c01836a15b4e6f951
Simon-Njoroge <mukirisimon22@gmail.com>
2024-05-20 16:37:44 +0300

    tsx first project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function sayhi(){
+    console.log("hello tsx");
+    function myfunction(){
+        console.log("signing you in")
+    }
+    let signyou =setTimeout(myfunction,3000,);
+    
+}
+sayhi();
---
+++ tojs/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayhi() {
+    console.log("hello tsx");
+}
+sayhi();
